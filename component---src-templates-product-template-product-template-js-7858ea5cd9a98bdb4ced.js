7e4f8df686eeb0dafa309d30dbed32a1d1fd99f3
danleks <2293601+danleks@users.noreply.github.com>
2019-11-10 21:56:27 +0100

    Updates

---
+++ component---src-templates-product-template-product-template-js-7858ea5cd9a98bdb4ced.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{241:function(e,t,a){"use strict";a.r(t),a.d(t,"query",(function(){return d}));var n=a(0),r=a.n(n),u=a(238),l=a.n(u),d="2695606549";t.default=function(e){var t=e.data.markdownRemark;return r.a.createElement("div",null,r.a.createElement("h2",null,t.frontmatter.title),r.a.createElement(l.a,{fluid:t.frontmatter.featuredImage.childImageSharp.fluid}))}}}]);
+//# sourceMappingURL=component---src-templates-product-template-product-template-js-7858ea5cd9a98bdb4ced.js.map
---
+++ styles-cea31bbf5d89d8f72d5b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{189:function(n,w,o){}}]);
+//# sourceMappingURL=styles-cea31bbf5d89d8f72d5b.js.map
